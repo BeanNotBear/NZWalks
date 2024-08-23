e8c43caa4b569c6ea18e6671b3be15201e003adf
Nguyen Van Nghi <nghinv03@gmail.com>
2024-08-23 13:58:37 +0700

    Get all regions

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+	// https://localhost:portnumber/api/regions
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegionsController : ControllerBase
+	{
+
+		private readonly NZWalksDbContext _dbcontext;
+
+		public RegionsController(NZWalksDbContext dbcontext)
+		{
+			_dbcontext = dbcontext;
+		}
+
+		// GET ALL REGIONS
+		// GET: https://localhost:portnumber/api/regions
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var regions = _dbcontext.Regions.ToList();
+			return Ok(regions);
+		}
+	}
+}
