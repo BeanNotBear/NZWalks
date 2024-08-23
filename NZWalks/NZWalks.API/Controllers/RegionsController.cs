51593729b425651e80c63c5f5d28a17915e9cf71
Nguyen Van Nghi <nghinv03@gmail.com>
2024-08-23 15:01:34 +0700

    Create a new Region

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
@@ -69,5 +70,35 @@ namespace NZWalks.API.Controllers
 
 			return Ok(regionDto);
 		}
+
+		// POST to Create a New Region
+		// POST: https://localhost:portnumber/api/regions
+		[HttpPost]
+		public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+		{
+			// Map DTO to Domain model
+			var regionDomainModel = new Region()
+			{
+				Code = addRegionRequestDto.Code,
+				Name = addRegionRequestDto.Name,
+				RegionImageUrl = addRegionRequestDto.RegionImageUrl
+			};
+
+			// Use Domain Model to create Region
+			_dbcontext.Regions.Add(regionDomainModel);
+			_dbcontext.SaveChanges();
+
+			// Map Domain Model to DTO
+			var regionDto = new RegionDto()
+			{
+				Id = regionDomainModel.Id,
+				Name = regionDomainModel.Name,
+				Code = regionDomainModel.Code,
+				RegionImageUrl = regionDomainModel.RegionImageUrl
+			};
+
+			return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+		}
+
 	}
 }
