962e70eec7e3e0ae2dfd35b12012787ba6bb2db3
Nguyen Van Nghi <nghinv03@gmail.com>
2024-09-01 23:18:37 +0700

    Test 2

--- NZWalks/NZWalks.API/Controllers/WalksController.cs
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -25,7 +25,7 @@ namespace NZWalks.API.Controllers
 		}
 
 		// CREATE Walk
-		// POST: /api/Walks
+		// POST: /api/walks
 		[HttpPost]
 		[ValidateModel]
 		public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
