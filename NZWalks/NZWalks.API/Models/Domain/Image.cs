a7607886bca9aaeae39a6c96962acbfb8f1197bb
Nguyen Van Nghi <nghinv03@gmail.com>
2024-09-05 09:23:06 +0700

    Create domain model for image upload

---
+++ NZWalks/NZWalks.API/Models/Domain/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+	public class Image
+	{
+		public Guid Id { get; set; }
+
+		[NotMapped]
+		public IFormFile File { get; set; }
+		public string FileName { get; set; }
+		public string? FileDescription { get; set; }
+		public string FileExtension { get; set; }
+		public long FileSizeBytes { get; set; }
+		public string FilePath { get; set; }
+	}
+}
