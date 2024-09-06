f8ab6f0774e5c8a90990e8273a138cac787528a6
NghiNV <nghinv03@gmail.com>
2024-09-06 22:37:29 +0700

    Get all Regions to UI (Call get all Regions API)

---
+++ NZWalks/NZWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+
+namespace NZWalks.UI.Controllers
+{
+	public class RegionsController : Controller
+	{
+		private readonly IHttpClientFactory httpClientFactory;
+		private readonly IConfiguration configuration;
+
+		public RegionsController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+		{
+			this.httpClientFactory = httpClientFactory;
+			this.configuration = configuration;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			List<RegionDto> response = new List<RegionDto>();
+			try
+			{
+				// Get all Regions from Web API
+				var client = httpClientFactory.CreateClient();
+
+				var httpResponseMessage = await client.GetAsync(configuration["Api:GetAllRegions"]);
+
+				httpResponseMessage.EnsureSuccessStatusCode();
+
+				response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());  
+			}
+			catch (Exception ex)
+			{
+				// Logging
+			}
+
+			return View(response);
+		}
+	}
+}
